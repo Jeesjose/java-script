326c154f6f01b07a094750a3b3ea916d019ba5d8
Jeesjose <150226742+Jeesjose@users.noreply.github.com>
2024-05-21 15:03:10 +0530

    Add files via upload

---
+++ FSD-JS-DAY-1-main/function.js
@@ -0,0 +1,14 @@
+// function add(a,b){
+//     let c= a+b;
+//     console.log(c);
+
+// }
+
+// add(1,2);
+
+function fullname(fname,lname){
+   
+    console.log(`${fname} ${lname}`);
+
+}
+fullname("Elias","Varghese");
---
+++ FSD-JS-DAY-1-main/script.js
@@ -0,0 +1,74 @@
+// posfix increment
+//let a= 1;
+//let b=a++;
+//console.log(a);
+//console.log(b);
+
+// prefix increment
+//let q=3;
+//let s=++q;
+//console.log(q);
+//console.log(s);
+// let car ={
+//     model:"bmw",
+//     color:"blue",
+// };
+
+// console.log(car.color);
+// let person = {
+//     fname: "Nelbin Nelson",
+//     address:{
+//         street:"gfdd",
+//         place:"ere",
+//         pin:321,
+//     },
+//     age:32
+// }
+// console.log(person.address.place)
+// var toothbrush=new Object();
+// toothbrush.name="colgate";
+// toothbrush.colour="blue";
+
+// console.log(toothbrush);
+
+// concatination
+// let fname= "Elias";
+// let lname= "Varghese";
+// console.log(`${fname} ${lname}`)
+
+// let m=5;
+// let n=6;
+// console.log(m+n)
+
+// Array
+// let fruits = ["Apple","Orange","mango",24]
+// console.log(fruits)
+// fruits.push("Grape")
+// console.log(fruits.length)
+// fruits[2]="Pineapple";
+// console.log(fruits)
+// fruits.unshift("Pears")
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+// Array of object
+// let bsc = [
+//     {
+//         fname: "Anto",
+//         age: 10,
+//     },
+    
+//     {
+//         fname: "Nelbin",
+//         age: 20,
+//     },
+
+//     {
+//         fname: "Hari",
+//         age: 30,
+//     },
+// ]
+// console.log(bsc[2].age);
